9b2f202187910c0ee2af3704b7b78d58ff05fdc3
Eoin Feely <me@eoinfeely.com>
2023-04-04 21:20:08 +0300

    Update with Viveo Health case study

--- src/components/CSBody/CSBody.component.jsx
+++ src/components/CSBody/CSBody.component.jsx
@@ -49,64 +49,66 @@ const CSBody = ({
             </div>
         </div>
 
-        <div className="cs-body-content">
-            <div className="content">
-                <h3>
-                    Introduction
-                </h3>
-                <div className="two-col">
-                    <div className="col-small">
-                        <div className="col-small-content mb-10">
-                            <p className="med upper green">
-                                Client
-                            </p>
-                            <p className="med">
-                                {pageTitle}
-                            </p>
+        { myRole01 ? 
+            <div className="cs-body-content">
+                <div className="content">
+                    <h3>
+                        Introduction
+                    </h3>
+                    <div className="two-col">
+                        <div className="col-small">
+                            <div className="col-small-content mb-10">
+                                <p className="med upper green">
+                                    Client
+                                </p>
+                                <p className="med">
+                                    {pageTitle}
+                                </p>
+                            </div>
+                            <div class="col-small-content mb-10">
+                                <p className="med upper green">
+                                    Design Team
+                                </p>
+                                <p className="med">
+                                    {designTeam}
+                                </p>
+                            </div>
+                            <div class="col-small-content mb-10">
+                                <p className="med upper green">
+                                    Duration
+                                </p>
+                                <p className="med">
+                                    {duration}
+                                </p>
+                            </div>
                         </div>
-                        <div class="col-small-content mb-10">
-                            <p className="med upper green">
-                                Design Team
-                            </p>
-                            <p className="med">
-                                {designTeam}
-                            </p>
-                        </div>
-                        <div class="col-small-content mb-10">
-                            <p className="med upper green">
-                                Duration
-                            </p>
-                            <p className="med">
-                                {duration}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col-large">
-                        <div className="col-large-content">
-                            <p className=" med upper green">
-                                My role
-                            </p>
-                            <p className="med">
-                                <ul>
-                                    <li>
-                                        {myRole01}
-                                    </li>
-                                    <li>
-                                        {myRole02}
-                                    </li>
-                                    <li>
-                                        {myRole03}
-                                    </li>
-                                    <li>
-                                        {myRole04}
-                                    </li>
-                                </ul>
-                            </p>
+                        <div className="col-large">
+                            <div className="col-large-content">
+                                <p className=" med upper green">
+                                    My role
+                                </p>
+                                <p className="med">
+                                    <ul>
+                                        <li>
+                                            {myRole01}
+                                        </li>
+                                        <li>
+                                            {myRole02}
+                                        </li>
+                                        <li>
+                                            {myRole03}
+                                        </li>
+                                        <li>
+                                            {myRole04}
+                                        </li>
+                                    </ul>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        : null }
 
         {/* <div className="cs-body-content">
             <h3>
@@ -115,57 +117,66 @@ const CSBody = ({
             <GatsbyImage image={theProcess} alt="TEST" />
         </div> */}
 
-        <div className="cs-body-content">
-            <div className="section">
-                <h3>UX research</h3>
-                <p className="med mb-10">
-                    {uxResearch01}
-                </p>
-                <p className="med">
-                    {uxResearch02}
-                </p>
-            </div>
+        { uxResearch01 ? 
+            <div className="cs-body-content">
+                <div className="section">
+                    <h3>UX research</h3>
+                    <p className="med mb-10">
+                        {uxResearch01}
+                    </p>
+                    <p className="med">
+                        {uxResearch02}
+                    </p>
+                </div>
 
-            <div className="section mt-50">
-                <h3>
-                    Information Architecture
-                </h3>
-                <GatsbyImage image={iaImage} alt="TEST" />
-                
-            </div>
+                { iaImage ?
+                    <div className="section mt-50">
+                        <h3>
+                            Information Architecture
+                        </h3>
+                        <GatsbyImage image={iaImage} alt="image" />
+                        
+                    </div>
+                : null }
 
-            <div className="section">
-                <h3>
-                    User flows
-                </h3>
-                <GatsbyImage image={userFlows} alt="TEST" />
-            </div>
-        </div>
+                { userFlows ? 
+                    <div className="section">
+                        <h3>
+                            User flows
+                        </h3>
+                        <GatsbyImage image={userFlows} alt="image" />
+                    </div>
+                : null }
 
-        <div className="cs-body-content">
-            <div className="content">
-                <h3>
-                    Wireframes
-                </h3>
-                <div className="three-col">
-                    <div className="col image">
-                        <GatsbyImage image={wireframesImg01} alt="TEST" />
-                    </div>
-                    <div className="col image">
-                        <GatsbyImage image={wireframesImg02} alt="TEST" />
-                    </div>
-                    <div className="col image">
-                        <GatsbyImage image={wireframesImg03} alt="TEST" />
-                    </div>
-                    <div className="col image">
-                        <GatsbyImage image={wireframesImg04} alt="TEST" />
-                    </div>
-                    <div className="col image">
-                        <GatsbyImage image={wireframesImg05} alt="TEST" />
+            </div>
+        : null }
+
+        { wireframesImg01 ? 
+            <div className="cs-body-content">
+                <div className="content">
+                    <h3>
+                        Wireframes
+                    </h3>
+                    <div className="three-col">
+                        <div className="col image">
+                            <GatsbyImage image={wireframesImg01} alt="TEST" />
+                        </div>
+                        <div className="col image">
+                            <GatsbyImage image={wireframesImg02} alt="TEST" />
+                        </div>
+                        <div className="col image">
+                            <GatsbyImage image={wireframesImg03} alt="TEST" />
+                        </div>
+                        <div className="col image">
+                            <GatsbyImage image={wireframesImg04} alt="TEST" />
+                        </div>
+                        <div className="col image">
+                            <GatsbyImage image={wireframesImg05} alt="TEST" />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        : null }
 
         {/* <div className="cs-body-content">
             <div className="content">
@@ -178,105 +189,112 @@ const CSBody = ({
             </div>
         </div> */}
 
-        <div className="cs-body-content">
-            <div className="content">
-                <h3>
-                    Final design
-                </h3>
-                <GatsbyImage image={finalDesign} alt="TEST" className="final-image" />
-                <Link to={prototypeURL} className="cta" target="_blank">
-                    {pageCTA}
-                </Link>
-
-                <div className="section mt-50">
+        { finalDesign ? 
+            <div className="cs-body-content">
+                <div className="content">
                     <h3>
-                        UI Library
+                        Final design
                     </h3>
-                    <div className="two-col mb-10">
-                        <div className="col">
-                            <p className="bold">
-                                Fonts used
-                            </p>
+                    <GatsbyImage image={finalDesign} alt="TEST" className="final-image" />
+                    <Link to={prototypeURL} className="cta" target="_blank">
+                        {pageCTA}
+                    </Link>
+
+                    <div className="section mt-50">
+                        <h3>
+                            UI Library
+                        </h3>
+                        <div className="two-col mb-10">
+                            <div className="col">
+                                <p className="bold">
+                                    Fonts used
+                                </p>
+                            </div>
+                            <div className="col ta-right">
+                                <p>
+                                    {fontsUsed01}
+                                </p>
+                                <p>
+                                    {fontsUsed02}
+                                </p>
+                                <p>
+                                    {fontsUsed03}
+                                </p>
+                                {fontsUsed04 ?
+                                    <p>
+                                        {fontsUsed04}
+                                    </p>
+                                : null}
+                            </div>
                         </div>
-                        <div className="col ta-right">
-                            <p>
-                                {fontsUsed01}
-                            </p>
-                            <p>
-                                {fontsUsed02}
-                            </p>
-                            <p>
-                                {fontsUsed03}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="two-col mt-50">
-                        <div className="col">
-                            <p className="bold">
-                                Colours used
-                            </p>
-                        </div>
-                        <div className="col ui-colours">
-                            <div className="colour-block">
-                                <div 
-                                    className="colour" 
-                                    style={{ 
-                                        backgroundColor: `${colorsUsed01}` 
-                                    }} 
-                                />
-                                <p className="hex">
-                                    {colorsUsed01}
+                        <div className="two-col mt-50">
+                            <div className="col">
+                                <p className="bold">
+                                    Colours used
                                 </p>
                             </div>
-                            <div className="colour-block">
-                                <div 
-                                    className="colour" 
-                                    style={{ 
-                                        backgroundColor: `${colorsUsed02}` 
-                                    }} 
-                                />
-                                <p className="hex">
-                                    {colorsUsed02}
-                                </p>
-                            </div>
-                            <div className="colour-block">
-                                <div 
-                                    className="colour" 
-                                    style={{ 
-                                        backgroundColor: `${colorsUsed03}` 
-                                    }} 
-                                />
-                                <p className="hex">
-                                    {colorsUsed03}
-                                </p>
-                            </div>
-                            <div className="colour-block">
-                                <div 
-                                    className="colour" 
-                                    style={{ 
-                                        backgroundColor: `${colorsUsed04}` 
-                                    }} 
-                                />
-                                <p className="hex">
-                                    {colorsUsed04}
-                                </p>
-                            </div>
-                            <div className="colour-block">
-                                <div 
-                                    className="colour" 
-                                    style={{ 
-                                        backgroundColor: `${colorsUsed05}` 
-                                    }} 
-                                />
-                                <p className="hex">
-                                    {colorsUsed05}
-                                </p>
+                            <div className="col ui-colours">
+                                <div className="colour-block">
+                                    <div 
+                                        className="colour" 
+                                        style={{ 
+                                            backgroundColor: `${colorsUsed01}` 
+                                        }} 
+                                    />
+                                    <p className="hex">
+                                        {colorsUsed01}
+                                    </p>
+                                </div>
+                                <div className="colour-block">
+                                    <div 
+                                        className="colour" 
+                                        style={{ 
+                                            backgroundColor: `${colorsUsed02}` 
+                                        }} 
+                                    />
+                                    <p className="hex">
+                                        {colorsUsed02}
+                                    </p>
+                                </div>
+                                <div className="colour-block">
+                                    <div 
+                                        className="colour" 
+                                        style={{ 
+                                            backgroundColor: `${colorsUsed03}` 
+                                        }} 
+                                    />
+                                    <p className="hex">
+                                        {colorsUsed03}
+                                    </p>
+                                </div>
+                                <div className="colour-block">
+                                    <div 
+                                        className="colour" 
+                                        style={{ 
+                                            backgroundColor: `${colorsUsed04}` 
+                                        }} 
+                                    />
+                                    <p className="hex">
+                                        {colorsUsed04}
+                                    </p>
+                                </div>
+                                <div className="colour-block">
+                                    <div 
+                                        className="colour" 
+                                        style={{ 
+                                            backgroundColor: `${colorsUsed05}` 
+                                        }} 
+                                    />
+                                    <p className="hex">
+                                        {colorsUsed05}
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        : null }
 
     </div>
 );
